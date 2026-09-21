77505b18033c8832a185d7d21f06c70f6acea7ed
Nyerguds <nyerguds@gmail.com>
2026-09-21 12:20:04 +0200

    MissionNameChecker v1.0.0.0

---
+++ CnCMixNameFinder/domain/NameGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MissionNameChecker.domain
+{
+    class NameGenerator
+    {
+
+        /// <summary>
+        /// Generates an ID as it is stored in a C&amp;C1 type mixfile.
+        /// The input is always changed to upper case,
+        /// making the algorithm case insensitive.
+        /// Original C++ code: http://xhp.xwis.net/documents/MIX_Format.html
+        /// </summary>
+        /// <param name="name">Filename to encode</param>
+        /// <returns>the hashed ID of the filename</returns>
+        public static UInt32 getNameId(String name)
+        {
+            name = name.ToUpper();              // convert to uppercase
+            int i = 0;
+            UInt32 id = 0;
+            int l = name.Length;          // length of the filename
+            while (i < l)
+            {
+                UInt32 a = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    a >>= 8;
+                    if (i < l)
+                        a += ((UInt32)name[i] << 24);
+                    i++;
+                }
+                id = (id << 1 | id >> 31) + a;
+            }
+            return id;
+        }
+
+        public static String getNameIdHexString(String name)
+        {
+            return getNameId(name).ToString("X4").PadLeft(8, '0');
+        }
+    }
+}
---
+++ CnCMixNameFinder/idcalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MissionNameChecker.domain;
+
+namespace MissionNameChecker
+{
+    public partial class idcalc : Form
+    {
+        public idcalc()
+        {
+            InitializeComponent();
+            button1_Click(this, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = NameGenerator.getNameIdHexString(textBox1.Text);
+        }
+    }
+}
